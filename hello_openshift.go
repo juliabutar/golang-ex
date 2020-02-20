360287cd143a177cf315e087f4faeb69e9f15136
juliabutar <julia.17butarbutar@gmail.com>
2020-02-20 10:23:44 +0700

    Update hello_openshift.go

--- hello_openshift.go
+++ hello_openshift.go
@@ -9,7 +9,7 @@ import (
 func helloHandler(w http.ResponseWriter, r *http.Request) {
 	response := os.Getenv("RESPONSE")
 	if len(response) == 0 {
-		response = "Hello OpenShift!"
+		response = "Selamat pagii wkkw!"
 	}
 
 	fmt.Fprintln(w, response)
